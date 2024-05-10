7f66554b0a006caa7b4060896bc94c36c3c69a97
Grirgory Rylov <grishberg@gmail.com>
2024-05-10 19:44:03 +0300

    added lambda to linearExtrude

--- src/main/kotlin/kotlinOpenScad/extension/ScadModuleBuilderExtrude.kt
+++ src/main/kotlin/kotlinOpenScad/extension/ScadModuleBuilderExtrude.kt
@@ -2,7 +2,6 @@ package kotlinOpenScad.extension
 
 import kotlinOpenScad.core.ScadCode
 
-
 fun ScadCode.linearExtrude(
     height: Number?,
     center: Boolean? = null,
@@ -30,6 +29,30 @@ fun ScadCode.linearExtrude(
     convexity: Number? = null,
     twist: Number? = null,
     slices: Int? = null,
+    scale: Number? = null,
+    resolution: Int? = null,
+    body: ScadCode.() -> Unit,
+) {
+    val params = _buildParams(
+        "height" to height,
+        "center" to center,
+        "convexity" to convexity,
+        "twist" to twist,
+        "slices" to slices,
+        "scale" to scale,
+        "\$fn" to resolution,
+    )
+    return _buildGroup("linear_extrude($params)") {
+        _createCleanScope().body()
+    }
+}
+
+fun ScadCode.linearExtrude(
+    height: Number?,
+    center: Boolean? = null,
+    convexity: Number? = null,
+    twist: Number? = null,
+    slices: Int? = null,
     scaleX: Number,
     scaleY: Number,
     resolution: Int? = null,
@@ -46,7 +69,30 @@ fun ScadCode.linearExtrude(
     return _extend("linear_extrude($params)")
 }
 
-
+fun ScadCode.linearExtrude(
+    height: Number?,
+    center: Boolean? = null,
+    convexity: Number? = null,
+    twist: Number? = null,
+    slices: Int? = null,
+    scaleX: Number,
+    scaleY: Number,
+    resolution: Int? = null,
+    body: ScadCode.() -> Unit,
+) {
+    val params = _buildParams(
+        "height" to height,
+        "center" to center,
+        "convexity" to convexity,
+        "twist" to twist,
+        "slices" to slices,
+        "scale" to _buildParamsArray(scaleX, scaleY),
+        "\$fn" to resolution,
+    )
+    return _buildGroup("linear_extrude($params)") {
+        _createCleanScope().body()
+    }
+}
 
 fun ScadCode.rotateExtrude(
     convexity: Number? = null,
@@ -63,4 +109,24 @@ fun ScadCode.rotateExtrude(
         "\$fn" to fragmentCount,
     )
     return _extend("rotate_extrude($params)")
+}
+
+fun ScadCode.rotateExtrude(
+    convexity: Number? = null,
+    angle: Number? = null,
+    fragmentMinimumAngle: Number? = null,
+    fragmentMinimumLength: Number? = null,
+    fragmentCount: Int? = null,
+    body: ScadCode.() -> Unit,
+) {
+    val params = _buildParams(
+        "convexity" to convexity,
+        "angle" to angle,
+        "\$fa" to fragmentMinimumAngle,
+        "\$fs" to fragmentMinimumLength,
+        "\$fn" to fragmentCount,
+    )
+    return _buildGroup("rotate_extrude($params)") {
+        _createCleanScope().body()
+    }
 }
